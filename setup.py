c06070aabf336e817266ee1df4328bb0339eaf3e
romande <rdembits@synamedia.com>
2021-05-12 19:51:02 -0700

    increased version to 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aws-session-management",
-    version="0.0.6",
+    version="0.0.7",
     author="Roman Dembitsky",
     author_email="romande@gmail.com",
     description="AWS Session Management",
